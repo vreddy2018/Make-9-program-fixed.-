454c6758806f857e7cf6960d6507f63b1e005a4d
Neil Reddy <vreddy2018@pingry.org>
2016-03-03 11:49:49 -0500

    Changed x variable to bust. Added invalid input if user types something other than 'y' or 'n' when prompted to play. Fixed comments.

--- make9.py
+++ make9.py
@@ -1,10 +1,8 @@
-#Author: Neil Reddy
-#Date: 2/13/16
-
 import random
 number = 0
-x = 0
-#this integer (x) will be used later on for when the user no longer wants a number added, but is still under 9.
+
+bust = False
+#this will keep track if the player busts
 
 print("This game is essentially blackjack, but you want to reach 9, not 21. Also the numbers being added are any number between 0 and 3, including decimals.\nIf you want a hit (get another number added to your score) type y for yes when prompted.\nIf you think you are close enough to 9 and are worried that another number will make you go over then respond with n for no.\nYou start with 0 points.\nLet's begin.")
 
@@ -20,27 +18,29 @@ if play == "y":
 				print("You have {} points".format(round(number, 2)))
 	
 		elif more == "n":
-			x = 1
-	#This sets x to 1 so later on the "you lose" statement will not print. 
+
+
 	
 			if number>8:
 				print("Final score: {}. Nice job.".format(round(number, 2)))
+				exit()
 	
 			else:
 				print("Final score: {}. You were not that close. You should have kept adding numbers".format(round(number, 2)))
-		
-			number = number + 9.1
-	#Because the user ended the game while the number is still under 9, the program would keep looping even though the user does not want any more numbers. Therefore, by adding 9.1 to whatever the user's number is, the number will automatically be over 9 and thus end the while loop. 
-
+				exit()
+	
 		else:
 			print("Invalid input. Response has to be either y or n")
-else:
-	print("You're missing out")
-	x = 1
+	
+	bust = True #sets bust to true when it goes over
 
-if x == 0:
-#x will stay 0 if the person goes over 9, but if the person is under 9 and no longer wants a hit, x is set to 1, so this losing statement will not be printed.
-	print("You lose. You went over 9. Your number was {}.".format(round(number, 2)))
+elif play == 'n':
+	print("You're missing out")
+
 else:
-	pass
-#pass does nothing
+	print("Invalid input. Response has to be either y or n. Play again")
+
+
+if bust:
+
+	print("You lose. You went over 9. Your number was {}.".format(round(number, 2)))
